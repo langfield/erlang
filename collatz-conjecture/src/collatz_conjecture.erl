dbb7ee40baaea9f62a7ea6fee1134fc55cc9ac56
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2024-05-14 05:09:41 -0400

    Add tail recursive version of `collatz`

--- collatz-conjecture/src/collatz_conjecture.erl
+++ collatz-conjecture/src/collatz_conjecture.erl
@@ -2,8 +2,11 @@
 
 -export([steps/1]).
 
-
 steps(1) -> 0;
 steps(N) when N =< 1 -> error(badarg);
-steps(N) when N rem 2 == 0 -> 1 + steps(N div 2);
-steps(N) -> 1 + steps(3 * N + 1).
+steps(N) -> steps(N, 0).
+
+%% Tail-recursive helper function.
+steps(1, Acc) -> Acc;
+steps(N, Acc) when N rem 2 == 0 -> steps(N div 2, Acc+1);
+steps(N, Acc) -> steps(3*N + 1, Acc+1).
